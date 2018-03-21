7f1ee69f523439bffb317283709e9a6bed5c67fd
DESKTOP-V06510B\diogo <diogoqueirooz1212@gmail.com>
2018-03-21 00:28:43 -0300

    Create two functions, PrintIntro and GetGuessAndPrintBack responsible for printing the introduction to the isogram game, and GetGuessAndPrint is responsible to get the Guess from the player and showing back.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,25 +3,34 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
 int main ()
 {
-	constexpr int WORD_LENGTH = 5;
-
-	cout << "Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORD_LENGTH <<" letter isogram I'm thinking of?\n";
-	cout << endl;
+	PrintIntro();
 
 	// Get a Guess from the Player
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+	return 0;
+}
+
+void PrintIntro()
+{
+	// Introduce the game 
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+string GetGuessAndPrintBack()
+{
 	string Guess = "";
 	cout << "Enter your Guess: ";
 	getline(cin, Guess);
-	// Repeat the Guess back to them
 	cout << "Your Guess was: " << Guess << endl;
-	// Get a Guess from the Player
-	cout << "Enter your Guess: ";
-	getline(cin, Guess);
-	// Repeat the Guess back to them
-	cout << "Your Guess was: " << Guess << endl;
-
-	return 0;
+	return Guess;
 }
