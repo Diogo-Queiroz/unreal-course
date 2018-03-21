c50ee7c692e17fbbcab34a892684ff15f9958d34
DESKTOP-V06510B\diogo <diogoqueirooz1212@gmail.com>
2018-03-20 23:53:59 -0300

    Now we are able to get the Guess from the player using getline and std::cin, and show it back.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main ()
 {
 	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH <<" letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	// Get a Guess from the Player
+	string Guess = "";
+	cout << "Enter your Guess: ";
+	getline(cin, Guess);
+	// Repeat the Guess back to them
+	cout << "Your Guess was: " << Guess << endl;
+	// Get a Guess from the Player
+	cout << "Enter your Guess: ";
+	getline(cin, Guess);
+	// Repeat the Guess back to them
+	cout << "Your Guess was: " << Guess << endl;
+
 	return 0;
 }
